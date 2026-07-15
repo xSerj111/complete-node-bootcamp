f9139024a35690be93b13ff883d5f9a681d31563
xSerj111 <187848408+xSerj111@users.noreply.github.com>
2026-07-15 16:01:41 +0200

    next commit

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 // //Blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -25,44 +27,62 @@ const url = require('url');
 //         console.log('File has been written');
 //       })
 //     });
-//   });  
+//   });
 // });
 // console.log('Will read file from file and then write to one');
 
 // SERVER
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8',
+);
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8',
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);    
+const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-const server =http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
   const pathName = req.url;
 
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is an overview');
-  } 
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  }
   // Product page
-  else if (pathName === '/product') {
-    res.end('This is product.');
-  } 
+  else if (pathname === '/product') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    console.log(query);
+    res.end(output);
+  }
   // API
-  else if (pathName === '/api') {    
-      res.writeHead(200, { 'content-type': 'application/json'})
-      res.end(data);
-    }
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(data);
+  }
   // Not found
   else {
     res.writeHead(404, {
       'content-type': 'text/html',
-      'my-own-header': 'hello-world'
-    })
+      'my-own-header': 'hello-world',
+    });
     res.end('<h1>Error. Wrong endpoint</h1>');
   }
-  
 });
 
 server.listen(8000, '127.0.0.1', () => {
