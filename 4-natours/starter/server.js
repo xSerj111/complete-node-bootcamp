a263d23e6224bc257d6505e3dd72238f99c82cb6
xSerj111 <187848408+xSerj111@users.noreply.github.com>
2026-08-25 09:20:00 +0200

    update

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -15,6 +15,36 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5  
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour ({
+  name: 'The Park Camper',
+  price: 997
+ 
+});
+
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('error: ', err);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
