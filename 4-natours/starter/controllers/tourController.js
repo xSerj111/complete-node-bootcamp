d06aec1ebe3f59f96a67303d76d3428040b80cc1
xSerj111 <187848408+xSerj111@users.noreply.github.com>
2026-09-01 15:02:46 +0200

    update

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -107,7 +107,7 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
+    await Tour.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
       status: 'success',
