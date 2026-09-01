b528c22c9b3ba0c6091e77806d673fd701f0b096
xSerj111 <187848408+xSerj111@users.noreply.github.com>
2026-09-01 16:10:57 +0200

    update

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -35,7 +35,23 @@ const tourSchema = new mongoose.Schema({
   summary: {
     type: String,
     trim: true,
+    required: [true, 'A tour must have a summary'],
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
